5c358ee7bfa357c269601356b40752d835f1ba1b
shaurya2412 <shauryaa2412@gmail.com>
2025-01-07 12:19:22 +0530

    all the keys added in appwrite

---
+++ src/conf/conf.js
@@ -0,0 +1,16 @@
+
+const conf = {
+   
+
+    appWriteUrl: String(process.env.React_App_Appwrite_Url),
+    appWriteProjectId: String(process.env.React_App_Appwrite_Project_id),
+    appWriteDatabaseId: String(process.env.React_App_Appwrite_Database_ID),
+
+    appWriteCollectionId: String(process.env.React_App_Appwrite_Collection_Id),
+
+    appWriteBucketId: String(process.env.React_App_Appwrite_Bucket_Id),
+
+
+}
+
+export default conf
